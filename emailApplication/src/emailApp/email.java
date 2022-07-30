dd4d6d317d1d13b7aaebae1ed2b1c6e0dae52bba
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-07-30 13:32:50 -0300

    criando as classes e variáveis

---
+++ emailApplication/src/emailApp/email.java
@@ -0,0 +1,31 @@
+package emailApp;
+
+public class email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailBoxCapacity;
+	private String alternativeEmail;
+	
+	//first create the constructor to receive the first and last name
+	public email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATE: " + this.firstName + " " + this.lastName);
+	}
+	
+	
+	//ask for department
+	
+	
+	//Then generate Random password
+	
+	
+	//set the mailBoxCapacity
+	
+	//set the alternative email
+	
+	//after change the password
+
+}
---
+++ emailApplication/src/emailApp/emailApp.java
@@ -0,0 +1,10 @@
+package emailApp;
+
+public class emailApp {
+
+	public static void main(String[] args) {
+		email em1 = new email("John", "Smith");
+
+	}
+
+}
