31f8002422f4069c36f6e4f403ba4387ad0a4ebc
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-08-22 11:31:22 -0300

    alteração

--- emailApplication/src/emailApp/emailApp.java
+++ emailApplication/src/emailApp/emailApp.java
@@ -3,7 +3,7 @@ package emailApp;
 public class emailApp {
 
 	public static void main(String[] args) {
-		email em1 = new email("John", "Smith");
+		email em1 = new email("Beatriz", "Kuriki");
 		System.out.println(em1.showInfo());
 		
 	
